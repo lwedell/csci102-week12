c9511457610646343659434211946021c6d22e86
Liam Wedell <lwedell@mymail.mines.edu>
2019-11-21 22:44:15 -0700

    Added function ScoreFinder

--- Week12-utility.py
+++ Week12-utility.py
@@ -5,6 +5,7 @@
 
 def PrintOutput(string):
     print("OUTPUT %s" % string)
+    
 
 def LoadFile(file):
     with open(file, 'r') as read_file:
@@ -18,6 +19,7 @@ def LoadFile(file):
             
 
         return output_list
+    
 
 def UpdateString(a, b, index):
     string_list = list(a)
@@ -28,6 +30,7 @@ def UpdateString(a, b, index):
             
     new_string = "".join(string_list)
     PrintOutput(new_string)
+    
 
 def FindWordCount(l, s):
     count = 0
@@ -36,4 +39,19 @@ def FindWordCount(l, s):
             count += 1
 
     return count
+
     
+def ScoreFinder(list_1, list_2, string):
+    new_string = string.lower()
+    new_string = list(new_string)
+    new_string[0] = new_string[0].upper()
+    new_string = "".join(new_string)
+    
+    if new_string in list_1:
+        index = list_1.index(new_string)
+        score = list_2[index]
+        print("OUTPUT %s got a score of %d" % (new_string, score))
+
+    else:
+        print("OUTPUT player not found")
+        
