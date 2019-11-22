59bde76e3d719f38a5fca32e1998c671c818cd16
Liam Wedell <lwedell@mymail.mines.edu>
2019-11-21 22:13:26 -0700

    Added function LoadFile

--- Week12-utility.py
+++ Week12-utility.py
@@ -6,3 +6,15 @@
 def PrintOutput(string):
     print("OUTPUT %s" % string)
 
+def LoadFile(file):
+    with open(file, 'r') as read_file:
+        output_list = []
+        contents = read_file.readlines()
+        for line in contents:
+            if line[-1] == '\n':
+                output_list.append(line[:-1])
+            else:
+                output_list.append(line)
+            
+
+        return output_list
